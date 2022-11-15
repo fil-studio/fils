b360c02cba7048ed8c7e98de127852134b050881
Martí Fenosa <marti.fenosa@gmail.com>
2022-11-15 09:56:11 +0100

    WIP fix deploy

--- nomad/src/Nomad.ts
+++ nomad/src/Nomad.ts
@@ -1,3 +1,39 @@
+const linkRule = 'a:not([target]):not([href^=\\#]):not([fil-nomad-ignore])';
+
 export default class Nomad {
+	constructor(){
+
+		console.log('Entra');
+		
+		this.attachLinks();
+
+	}
 	
+	attachLinks(){
+		const domLinks = document.querySelectorAll(linkRule) as NodeListOf<HTMLLinkElement>;
+		for(const link of domLinks) {
+			link.addEventListener('click', this.navigate.bind(this));
+		}
+
+	}
+
+	dettachLinks(){
+		const domLinks = document.querySelectorAll(linkRule) as NodeListOf<HTMLLinkElement>;
+		for(const link of domLinks) {
+			link.removeEventListener('click', this.navigate.bind(this));
+		}
+	}
+
+	click(){
+
+	}
+
+	navigate(e){
+		if (e.metaKey || e.ctrlKey) return;
+
+		e.preventDefault();
+
+		console.log('Hola', e);
+		
+	}
 }
