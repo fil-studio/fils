843d399dfa1d78a787c23cbc0dbe0bfbd09a74ed
Edu Prats <eduprats@gmail.com>
2022-12-15 12:28:25 +0100

    Refactored packages with workspaces

--- scroller/lib/math/src/MathUtils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class MathUtils {
-    static clamp(v: number, min: number, max: number): number;
-    static lerp(v1: number, v2: number, alpha: number): number;
-    static mix(v1: number, v2: number, alpha: number): number;
-    static smoothstep(min: number, max: number, val: number): number;
-    static step(thrsh: number, val: number): number;
-    static map(x: number, a: number, b: number, c: number, d: number): number;
-    static fract(n: number): number;
-}
