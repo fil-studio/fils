a85d3e979912df24ee4098e853b8c7363b06b2dc
Martí Fenosa <marti.fenosa@gmail.com>
2023-01-17 15:13:32 +0100

    Upgrade margins and placement

--- packages/ui/src/core/UIElement.ts
+++ packages/ui/src/core/UIElement.ts
@@ -1,4 +1,5 @@
 import { Group } from "../components/Group";
+import { ALL_CLASS } from "./globals";
 
 export class UIElement {
 	dom: HTMLElement;
@@ -9,6 +10,7 @@ export class UIElement {
 	constructor(parent: Group ) {
 		this.parent = parent;
 		this.dom = document.createElement('div');
+		this.dom.classList.add(ALL_CLASS)
 		if(parent) {
 			parent.dom.appendChild(this.dom);
 			this.depth = parent.depth + 1;
--- packages/ui/src/core/globals.ts
+++ packages/ui/src/core/globals.ts
@@ -1,3 +1,5 @@
+export const ALL_CLASS = 'fil__ui-element';
+
 export const WRAPPER_CLASS = 'fil__ui';
 export const GROUP_CLASS = 'fil__ui-group';
 export const ITEM_CLASS = 'fil__ui-item';
