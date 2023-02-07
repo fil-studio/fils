71ebfd484676785aee3c0cd5e2d9788399b73e80
Martí Fenosa <marti.fenosa@gmail.com>
2023-02-07 15:47:21 +0100

    Fixes

--- packages/ui/src/components/panels/FloatingPanel.ts
+++ packages/ui/src/components/panels/FloatingPanel.ts
@@ -1,6 +1,8 @@
 import { CSS_UI } from "../../partials/cssClasses";
 import { Panel } from "./Panel";
 
+const MARGIN = 5;
+
 export class FloatingPanel extends Panel {
 	position: 'right' | 'left';
 
@@ -9,17 +11,22 @@ export class FloatingPanel extends Panel {
 
 		this.dom.el.classList.add(CSS_UI.panel.floating);
 
+		const uiR = this.dom.parent.el.closest(`.${CSS_UI.wrapper}`).getBoundingClientRect();
 		const r = this.dom.parent.el.getBoundingClientRect();
 
 		// For transition
 		let position = r.left + (r.width * .5) < window.innerWidth * .5 ? 'right' : 'left';
 		this.dom.el.classList.add(`_ui-panel-${position}`);
 
-		const offset = position === 'right' ? r.width : r.width * -1;
-
+		const offset = position === 'right' ? uiR.width + MARGIN : -uiR.width - MARGIN;
 
 		this.dom.el.style.top = `${r.top + (r.height * .5)}px`;
 		this.dom.el.style.left = `${r.left + (r.width * .5) + offset}px`;
+
+		const checkRect = this.dom.el.getBoundingClientRect();
+		console.log(checkRect);
+
+
 
 	}
 
