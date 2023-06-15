2164b0f5ca19ec4bf9e3254f2146baf57d15110c
Martí Fenosa <marti.fenosa@gmail.com>
2023-06-15 19:37:01 +0200

    WIP logo

--- examples/src/logo/js/App.ts
+++ examples/src/logo/js/App.ts
@@ -1,58 +1,48 @@
 import { gsap } from 'gsap';
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
+import { Vec } from "@fils/math";
+import { UI } from '@fils/ui';
+
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-type lineType = {
-	distance: number,
-	x: number,
-	y: number
+
+const COORDINATES:Vec[] = [
+	new Vec( -59, 176 ),
+	new Vec( -80, 268 ),
+	new Vec( 213, 268 ),
+	new Vec( 274, -267 ),
+	new Vec( 178, 85 ),
+	new Vec( -31, 85 ),
+	new Vec( 62, -267 ),
+	new Vec( -232, 268 ),
+	new Vec( -89, -267 ),
+	new Vec( -275, -126 ),
+];
+
+const DURATIONS:number[] = [];
+
+const TOTAL_DURATION = () => {
+	let total = 0;
+
+	for(let i = 1; i<COORDINATES.length; i++){
+		const v1 = COORDINATES[i];
+		const v2 = COORDINATES[i + 1] ? COORDINATES[i + 1] : new Vec(0,0);
+		const d = v1.distanceTo(v2);
+		total += d;
+		DURATIONS.push(d);
+	}
+
+	return total;
 }
-
-type bezierType = {
-	distance: number,
-	x: number,
-	y: number
-	controls: Array<{
-		x: number,
-		y: number
-	}>
-}
-
-function calculateDistance(x1: number, y1: number, x2: number, y2: number): number {
-	const xDistance = x2 - x1;
-	const yDistance = y2 - y1;
-
-	// Using Pythagorean theorem to calculate the distance
-	const distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-
-	return distance;
-}
-
-type Point = {
-	x: number;
-	y: number;
-}
-
-function calculateEndPoint(origin: Point, control1: Point, control2: Point): Point {
-	const P3 = {
-		x: (3 * control2.x) - (3 * control1.x) + origin.x,
-		y: (3 * control2.y) - (3 * control1.y) + origin.y
-	};
-	return P3;
-}
-
-
-const TOTAL_DURATION = 10;
 
 export class App {
 	path: SVGPathElement;
 	g: SVGElement;
 
 	tl: GSAPTimeline;
+	ui: UI;
 
-	positions: Array<lineType | bezierType> = [];
-	totalDistance: number = 0;
 	constructor() {
 		console.log('APP');
 
@@ -60,126 +50,121 @@ export class App {
 		this.g = document.querySelector('svg g') as SVGElement;
 
 
-		this.preparePath();
 		this.createTl();
 
 		window.addEventListener('click', () => {
-			this.tl.play(0)
+			// this.tl.play(0)
+		})
+
+		this.ui = new UI({
+			title: 'Paths',
+		})
+
+
+
+		const circle = document.querySelector('circle') as SVGElement;
+		const circlePosition = new Vec(500,500);
+		circle.setAttribute('cx', `${circlePosition.x}`)
+		circle.setAttribute('cy', `${circlePosition.y}`)
+
+		for(let i = 0; i<DURATIONS.length; i++){
+			const val = {
+				d: DURATIONS[i]
+			}
+			this.ui.add(val, 'd', {
+				title: 'Time' + val.d
+			}).on('change', () => {
+				DURATIONS[i] = val.d;
+			})
+		}
+
+
+		const p = {
+			progress: 0,
+		}
+		this.ui.add(p, 'progress', {
+			view: 'range',
+			min: 0,
+			max: 1000,
+			step: 1,
+		}).on('change', () => {
+			this.tl.progress(p.progress * 0.001)
+			// this.g.style.transform = `translate(${circlePosition.x}px, ${circlePosition.y}px)`;
+			// circle.setAttribute('cx', `${circlePosition.x}`)
+		})
+		// this.ui.add(circlePosition, 'y', {
+		// 	view: 'range',
+		// 	min: -1000,
+		// 	max: 1000,
+		// 	step: 1,
+		// }).on('change', () => {
+		// 	this.g.style.transform = `translate(${circlePosition.x}px, ${circlePosition.y}px)`;
+		// 	// circle.setAttribute('cy', `${circlePosition.y}`)
+		// })
+
+		this.ui.addButton('Reset TL', () => {
+			this.tl.pause(0);
+			this.createTl();
+			this.tl.play();
 		})
 
 	}
-
-	preparePath(){
-
-		const d = this.path.getAttribute('d')?.split(/[lcm]/i).filter(Boolean) as Array<string>;
-
-		let lastPoint:lineType = {
-			distance: 0,
-			x: 0,
-			y: 0
-		}
-
-		for(let i = 0; i < d?.length; i++){
-			const item = d[i];
-
-			const separe = item.split(',') as Array<string>;
-			const values = separe.map( val => parseFloat(val))
-
-			const line:lineType = {
-				distance: 0,
-				x: values[0],
-				y: values[1]
-			}
-
-			if(lastPoint.x == 0 && lastPoint.y == 0 && lastPoint.distance === 0) {
-				lastPoint = line;
-			}
-
-			if(values.length === 2){
-				line.distance = calculateDistance(lastPoint.x, lastPoint.y, line.x, line.y);
-				this.positions.push(line);
-			} else {
-				const controls:Point[] = [
-					{
-						x: values[2],
-						y: values[3]
-					},
-					{
-						x: values[4],
-						y: values[5]
-					},
-				]
-				const endpoint:Point = calculateEndPoint(line, controls[0], controls[1])
-				line.distance = calculateDistance(lastPoint.x, lastPoint.y, endpoint.x, endpoint.y);
-
-				const bezier:bezierType = {
-					...line,
-					controls,
-				}
-				this.positions.push(bezier);
-			}
-
-			this.totalDistance += line.distance;
-			lastPoint = line;
-
-
-		}
-
-		console.log(this.positions);
-	}
-
 
 	createTl(){
 
 		this.tl = gsap.timeline({
-			paused: false,
+			paused: true,
 			default: {
 				ease: 'linear'
+			},
+			onUpdate: () => {
+				console.log(this.tl.time());
+
 			}
 		})
 
-		// gsap.set(this.path, {
-		// 	drawSVG: '0%'
-		// })
+		gsap.set(this.path, {
+			drawSVG: '0%'
+		})
+		gsap.set(this.g, {
+			x: COORDINATES[0].x,
+			y: COORDINATES[0].y,
+		})
 
 
 		this.tl.addLabel('start');
 
-		for(let i = 0; i<this.positions.length; i++){
+		for(let i = 1; i<COORDINATES.length; i++){
 
-			const p = this.positions[i];
-
-			let duration = (p.distance / this.totalDistance) * TOTAL_DURATION;
-			console.log(duration, p.distance);
-
+			const coord = COORDINATES[i];
 
 			const tween = gsap.to(
 				this.g,
 				{
-					duration: duration,
+					duration: DURATIONS[i - 1],
 					ease: 'none',
-					x: p.x - 500,
-					y: p.y - 500
+					x: coord.x,
+					y: coord.y
 				}
 			);
 
-			this.tl.add(tween, '>')
+			this.tl.add(tween, i === 0 ? 'start' : '>')
 		}
 
 
 
 		this.tl
 			.to(this.path, {
-				duration: 9,
+				duration: TOTAL_DURATION,
 				drawSVG: '0% 100%',
 				ease: 'none'
 			}, 'start')
-			.to(this.g, {
-				duration: 1,
-				x: 0,
-				y: 0,
-				ease: 'none'
-			})
+			// .to(this.g, {
+			// 	duration: 1,
+			// 	x: 0,
+			// 	y: 0,
+			// 	ease: 'none'
+			// }, 'start')
 
 
 
