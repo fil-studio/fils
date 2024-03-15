6443b0b5d721b29eba8936dd075bd63399862e4e
Martí Fenosa <marti.fenosa@gmail.com>
2024-03-15 12:29:53 +0100

    Utils :: Breakthrough fullscreen

--- packages/scroller/src/partials/ScrollerEvents.ts
+++ packages/scroller/src/partials/ScrollerEvents.ts
@@ -127,7 +127,6 @@ export class ScrollerEvents {
 
 		target.addEventListener('touchmove', (e: TouchEvent) => {
 			if (this.blocked) return;
-			// e.preventDefault();
 			const e1 = e.touches[0];
 			touchWheel.delta = e1.clientY - touchWheel.startY;
 			touchWheel.startY = e1.clientY;
